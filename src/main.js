754510f8c393e7ab94f7d9602564394c1bce5a77
carlosbanales <carlos@7sixtymedia.com>
2022-02-10 00:46:15 -0800

    Started files, added input field, button

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const responses = [
+    "Yes",
+    "I say no",
+    "Mabye, someday",
+    "Not even magic can tell you that",
+    "There's a really good chance"
+]
+
+
+
+console.log(responses)
