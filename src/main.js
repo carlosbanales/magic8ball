c92e0449f4df10322fd2e3f35fefc4d1b6f015d8
carlosbanales <charliebananas42@gmail.com>
2022-02-10 18:39:07 -0800

    basic function complete

--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
+// var img = document.createElement("img");
+// img.src = "../assets/magic8ball_extra.png";
+// var src = document.getElementById("answers");
+// src.appendChild(img);
+
+// var images = [];
+// const index = 0;
+// images[0] = "../assets/1.png";
+// images[0] = "../assets/1.png";
+// images[0] = "../assets/1.png";
+// images[0] = "../assets/1.png";
+// images[0] = "../assets/1.png";
+// images[0] = "../assets/1.png";
+// images[0] = "../assets/1.png";
+// images[0] = "../assets/1.png";
+// images[0] = "../assets/1.png";
+
 function SwapImage(){
-    document.getElementById("8ball").src="../assets/magic8ball_2.png"
+    const index = Math.floor(Math.random() * 20);
+    console.log(index);
+    document.getElementById("8ball").src="../assets/" + index + ".png";
 }
