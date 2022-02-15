31739beea8bd71bc075a2504e623c1c7e53600d8
carlosbanales <charliebananas42@gmail.com>
2022-02-15 10:49:15 -0800

    made code easier to read

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
+//First attempt, was able to get the image to 
+// pop up but not cycle through the rest of the images
 
 // Creates image element and appends it to the div
 // var img = document.createElement("img");
 // img.src = "../assets/magic8ball_extra.png";
 // var src = document.getElementById("answers");
 // src.appendChild(img);
+
+// was able to cycle through the images,
 
 // var images = [];
 // const index = 0;
@@ -16,18 +20,24 @@
 // images[6] = "../assets/6.png";
 // images[7] = "../assets/7.png";
 // images[8] = "../assets/8.png";
+
+
 function imgShake() {
+    // shakes the image every other time
     document.getElementById("8ball").classList.toggle("shaking");
+    // create shake element to be able to addEventListener to end animation
+    var shakeElement = document.getElementById("8ball");
+    // use animationend to end the event
+    shakeElement.addEventListener('animationend', imgShake);
+    console.log(shakeElement);
 }
 
 function SwapImage(){
     imgShake();
-    var shakeElement = document.getElementById("8ball");
-    shakeElement.addEventListener('animationend', imgShake);
     // in order to get image 19.png to pop up, Math.random must
     // return .95 or greater which doesn't happen often
     const index = Math.floor(Math.random() * 20);
-    console.log(index);
+    // wait 500 ms to reassign the image object to a new image
     setTimeout(() => {
         document.getElementById("8ball").src="../assets/" + index + ".png";
     }, 500);
